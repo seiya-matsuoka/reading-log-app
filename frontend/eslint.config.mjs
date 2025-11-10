432009912743913cfc32b3c763ba06136d0eb452
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2025-11-10 13:33:14 +0900

    chore(frontend): ESLint / Prettier / Tailwind v4 を設定

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  { ignores: ['dist/**', 'node_modules/**'] },
+
+  js.configs.recommended,
+
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: { jsx: true },
+      globals: { ...globals.browser },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+
+  eslintConfigPrettier,
+];
