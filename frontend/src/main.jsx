eb4f451df5e0bb4c8546212f5d7d5846b6d68320
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2025-11-25 23:38:09 +0900

    feat(frontend): ToastProvider でアプリ全体をラップするように修正
    - main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles/tailwind.css';
 import App from './App.jsx';
 import { MeProvider } from './providers/meContext.jsx';
+import { ToastProvider } from './providers/ToastProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <MeProvider>
-        <App />
-      </MeProvider>
+      <ToastProvider>
+        <MeProvider>
+          <App />
+        </MeProvider>
+      </ToastProvider>
     </BrowserRouter>
   </StrictMode>
 );
